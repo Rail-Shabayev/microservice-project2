796cce12f1c4cb533159f3cade5011a430534c75
rail <rail.shabayev@mail.ru>
2024-03-01 07:40:51 +0400

    feat: add open api definition

---
+++ factory-service/src/main/java/org/rail/project/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package org.rail.project.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(version = "1.0",
+                summary = "Open Api definition of factory service",
+                title = "Factory service",
+                contact = @Contact(name = "rail", url = "https://github.com/Rail-Shabayev")),
+        servers = {
+                @Server(url = "http://locahost:8081",
+                        description = "default server of the service")
+        }
+
+)
+public class OpenApiConfig {
+}
