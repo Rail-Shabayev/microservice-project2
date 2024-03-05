0939867a3bd5bd92bc8474dea2faa9d6aee05dcb
rail <rail.shabayev@mail.ru>
2024-03-05 10:43:28 +0400

    feat: add routing for open api in gateway
    for entire 5 hours I just fucked with open api routing(┛^_^)┛彡<ǝlqɐʇ>

--- api-gateway/src/main/java/org/rail/ApiGatewayApp.java
+++ api-gateway/src/main/java/org/rail/ApiGatewayApp.java
@@ -1,11 +1,41 @@
 package org.rail;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(version = "2.0", title = "super title for my open api spec"))
 public class ApiGatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApp.class);
+    }
+
+    @Autowired
+    RouteDefinitionLocator locator;
+
+    /**
+     * this bean responsible for creating open api groupings
+     * @return list of open api groups from route definitions mapped inside application.yaml
+     */
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        assert definitions != null;
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service", "");
+            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+        });
+        return groups;
     }
 }
