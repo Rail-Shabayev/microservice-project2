d2b7edc48fb8599093d9f7cdf0577a035c1dfb61
rail <rail.shabayev@mail.ru>
2024-03-09 09:09:27 +0400

    feat: add caching to RestClient request

--- user-service/src/main/java/org/rail/controller/UserController.java
+++ user-service/src/main/java/org/rail/controller/UserController.java
@@ -3,6 +3,7 @@ package org.rail.controller;
 import lombok.RequiredArgsConstructor;
 import org.rail.model.User;
 import org.rail.repository.UserRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,7 @@ public class UserController {
 
     @GetMapping
     @Transactional(readOnly = true)
+    @Cacheable("users")
     public List<User> getAllUsers() {
         return restClient.get()
                 .retrieve()
