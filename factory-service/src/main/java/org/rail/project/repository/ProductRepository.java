85e58d8a137aa1d404773126b3bc685c3f5ce2ac
rail <rail.shabayev@mail.ru>
2024-03-16 10:27:45 +0400

    feat: add locks on top of productRepository methods
    in reality there's no working lock implementation

--- factory-service/src/main/java/org/rail/project/repository/ProductRepository.java
+++ factory-service/src/main/java/org/rail/project/repository/ProductRepository.java
@@ -1,18 +1,22 @@
 package org.rail.project.repository;
 
+import jakarta.persistence.LockModeType;
 import org.rail.project.model.Product;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Override
+    @Lock(LockModeType.NONE)
     @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "manufacturer")
     List<Product> findAll();
 
     @Override
+    @Lock(LockModeType.NONE)
     @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "manufacturer")
     Optional<Product> findById(Long aLong);
 
